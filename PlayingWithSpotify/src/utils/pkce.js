2bbb107fb699478f793bb9461b3b2837cebd427a
unknown <ok037352085@gmail.com>
2025-09-12 16:56:19 +0800

    update pkce

---
+++ PlayingWithSpotify/src/utils/pkce.js
@@ -0,0 +1,24 @@
+// utils/pkce.js
+export function generateCodeVerifier(length = 128) {
+  const chars = 'ABCDEFGHIJKLMNOPQRSTUVWXYZabcdefghijklmnopqrstuvwxyz0123456789-._~';
+  let verifier = '';
+  for (let i = 0; i < length; i++) {
+    verifier += chars.charAt(Math.floor(Math.random() * chars.length));
+  }
+  return verifier;
+}
+
+export async function generateCodeChallenge(verifier) {
+  const encoder = new TextEncoder();
+  const data = encoder.encode(verifier);
+  const digest = await crypto.subtle.digest('SHA-256', data);
+  return base64UrlEncode(digest);
+}
+
+function base64UrlEncode(buffer) {
+  let binary = '';
+  const bytes = new Uint8Array(buffer);
+  bytes.forEach(b => binary += String.fromCharCode(b));
+  return btoa(binary).replace(/\+/g, '-').replace(/\//g, '_').replace(/=+$/, '');
+}
+
